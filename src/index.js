93438f09c953cd7d7afec7fe961034494c093a6a
Ryan Folstad <ryan.folstad@gmail.com>
2017-11-15 11:29:22 +0700

    chore(stupid): fucking stupid

--- src/index.js
+++ src/index.js
@@ -15,7 +15,38 @@ const defaultsDeep = require('lodash.defaultsdeep');
 const merge = require('lodash.merge');
 const previewEmail = require('preview-email');
 
-const getPaths = require('get-paths');
+
+const path = require('path');
+const fs = require('fs-extra');
+
+
+function getPaths(abs, rel, ext) {
+  return fs
+    .stat(path.join(abs, rel))
+    .then(stats => {
+      if (stats.isDirectory()) {
+        // a directory
+        return {
+          rel: path.join(rel, `html.${ext}`),
+          ext
+        };
+      }
+      // a file
+      return { rel, ext: path.extname(rel).slice(1) };
+    })
+    .catch(err => {
+      // not a valid file/directory
+      if (!path.extname(rel) || path.extname(rel).slice(1) !== ext) {
+        // Template file has been provided without the right extension
+        // so append to it to try another lookup
+        return getPaths(abs, `${rel}.${ext}`, ext);
+      }
+      throw err;
+    });
+}
+
+
+
 const juiceResources = require('juice-resources-promise');
 
 class Email {
